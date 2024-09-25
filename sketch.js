7da6fd9f2e78d00f75548a5b9dd7b629013e8ca2
Ayo sus <116416379+MNA4@users.noreply.github.com>
2024-09-25 14:01:22 +0700

    ditched p5js, add collision (wip)

--- sketch.js
+++ sketch.js
@@ -12,8 +12,14 @@ class Vector2D {
     mult (n) {
         return new Vector2D(this.x * n, this.y * n);
     }
+    div(n) {
+        return new Vector2D(this.x / n, this.y / n);
+    }
     clone() {
         return new Vector2D(this.x, this.y);
+    }
+    dist_squared() {
+        return this.x**2 + this.y**2;
     }
 
 }
@@ -27,7 +33,7 @@ class World {
 }
 
 class Point {
-    constructor (world, pos, radius) {
+    constructor (world, pos, radius, mass = 1) {
         this.world = world;
         this.world.points.push(this);
 
@@ -35,30 +41,63 @@ class Point {
         this.prevPos = pos.clone();
 
         this.radius = radius;
+
+        this.mass = mass;
     }
     update (dt=1/30) {
         let tmp = this.pos.clone();
         this.pos = this.pos.add(this.pos.sub(this.prevPos).mult(this.world.frictionCoeff))
         this.pos.y += this.world.gravity * (dt**2);
-        this.pos.y = Math.min(this.pos.y, height - this.radius);
-        this.pos.x = Math.min(this.pos.x, width - this.radius);
+        this.pos.y = Math.min(this.pos.y, canvas.height - this.radius);
+        this.pos.x = Math.min(this.pos.x, canvas.width - this.radius);
         this.pos.x = Math.max(this.pos.x, this.radius);
         this.prevPos = tmp;
     }
+    collide(point) {
+        let d = point.pos.sub(this.pos);
+        let dd = d.dist_squared();
+        if (0 < dd < (this.radius + point.radius) ** 2) {
+            d = d.div(Math.sqrt(dd));
+            let i1 = this.pos.add(d.mult(this.radius));
+            let i2 = point.pos.add(d.mult(point.radius));
+            let midpoint = i1.mult(this.mass).add(i2.mult(point.mass)).div(this.mass+point.mass);
+            this.pos = midpoint.sub(d.mult(this.radius));
+            point.pos = midpoint.add(d.mult(point.radius));
+        }
+    }
     render () {
-        stroke('black');
-        strokeWeight(1);
-        circle(this.pos.x, this.pos.y, this.radius*2);
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 2;
+        ctx.fillStyle = 'white';
+        ctx.beginPath();
+        ctx.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI*2);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
     }
 }
+
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 600;
+canvas.height = 600;
+
 let world = new World();
-let p1 = new Point(world, new Vector2D(10, 10), 10);
-function setup() {
-    createCanvas(400, 400);
+new Point(world, new Vector2D(10, 10), 10);
+new Point(world, new Vector2D(30, 10), 10);
+new Point(world, new Vector2D(50, 10), 10);
+new Point(world, new Vector2D(70, 10), 10);
+
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (i of world.points) {
+        i.update();
+    }
+    for (i of world.points) {
+        i.render();
+    }
+    requestAnimationFrame(update);
 }
 
-function draw() {
-    background(200);
-    p1.update();
-    p1.render();
-}
+window.onload = update;
